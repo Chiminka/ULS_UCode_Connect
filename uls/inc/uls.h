093772b4f0430d9953598a3068717f0ea9d688f5
Mint_Caramel <91499156+LizaNilova@users.noreply.github.com>
2021-12-22 14:57:15 +0200

    Basic functionality
    Basic functionality, but without correct output

---
+++ uls/inc/uls.h
@@ -0,0 +1,21 @@
+#ifndef ULS_H_
+#define ULS_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <pwd.h>
+#include <errno.h>
+#include <grp.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include "libmx.h"
+
+#endif
